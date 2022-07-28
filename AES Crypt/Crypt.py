bc2fc975b0ee205baba52c56b4b633e2da46e5b4
Askold Apet <chiglok.ask@gmail.com>
2022-07-28 17:53:09 +1100

    key crypt

--- AES Crypt/Crypt.py
+++ AES Crypt/Crypt.py
@@ -1,8 +1,13 @@
+import crypt
 import hashlib
 import base64
+
+import Cryptodome.Cipher.AES
 from Cryptodome.Cipher import AES
 import os
 
-key = hashlib.sha3_256('123').digest()
-text = 'ТестовыйБрут'
-key_hash = AES.new(key, AES.MODE_CBC, )
+text = b'Brute'
+key = b'123'
+key_hash = AES.new(key, AES.MODE_CBC)
+ciphertext, tag = key_hash.encrypt_and_digest(text)
+print(ciphertext)
