b263c124ec7ef9b5e2e26b5f35d9909a92cfd22d
Askold Apet <chiglok.ask@gmail.com>
2022-07-28 17:26:08 +1100

    key crypt

--- AES Crypt/Crypt.py
+++ AES Crypt/Crypt.py
@@ -1,7 +1,6 @@
 import hashlib
-
-import Cryptodome.Cipher.AES
-from Cryptodome.Cipher import  AES
+import base64
+from Cryptodome.Cipher import AES
 import os
 
 key = hashlib.sha3_256('123').digest()
