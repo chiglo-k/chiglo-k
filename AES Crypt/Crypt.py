eb3ee53ff13dc58c78049d48471adbf0b6fc11b8
Askold Apet <chiglok.ask@gmail.com>
2022-08-02 00:20:38 +1100

    Crypt

--- AES Crypt/Crypt.py
+++ AES Crypt/Crypt.py
@@ -6,16 +6,18 @@ import Cryptodome.Cipher.AES
 from Cryptodome.Cipher import AES
 import os
 from hashlib import sha256
-
+from ftplib import FTP
 
 BS = AES.block_size
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 
 text = 'HomeWork'
+
 key = hashlib.sha256(b'123').digest()
 text = pad(text)
-iv = Random.new().read(BS)
-cipher = AES.new(key, AES.MODE_CBC, iv)
-crypt_text = (iv + cipher.encrypt(text.encode()))
+#iv = Random.new().read(BS)
+cipher = AES.new(key, AES.MODE_CBC)
+crypt_text = (cipher.encrypt(text.encode()))
 print('\nCipher:', crypt_text.hex())
+
 
